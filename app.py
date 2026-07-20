0586b240e51077e6e737cbe483092dfe2cc306f7
ghorpadesanika877-star <ghorpadesanika877@gmail.com>
2026-07-20 23:11:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import os
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 st.title("🎬 Movie Recommendation System")
 
